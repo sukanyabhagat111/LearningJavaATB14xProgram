9c67203f0c0d265c191cc88c9046ad53f55e89b4
sukanyabhagat <sukanyabhagat111@gmail.com>
2025-09-17 15:37:00 +0530

    13septclass

---
+++ src/LearningJavaATB14XProgram/Lab002_Comments.java
@@ -0,0 +1,12 @@
+package LearningJavaATB14XProgram;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+//        documentation
+        /**jhjhf
+                hjhf
+                jhkjf
+                        khhd
+                        kjhks **/
+    }
+}
