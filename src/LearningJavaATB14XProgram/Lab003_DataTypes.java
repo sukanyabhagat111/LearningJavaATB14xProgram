71517f59044f7ba3aa177314a6c7d9119ee2ad84
sukanyabhagat <sukanyabhagat111@gmail.com>
2025-09-17 17:52:52 +0530

    14Sept Printf task

---
+++ src/LearningJavaATB14XProgram/Lab003_DataTypes.java
@@ -0,0 +1,38 @@
+package LearningJavaATB14XProgram;
+
+public class Lab003_DataTypes {
+    public static void main(String[] args) {
+//        System.out.println("b");
+//
+//        int a = 9;
+//        System.out.println('0');
+
+
+        int N= 2;
+        System.out.println(N*4);
+
+int table =9;
+        System.out.printf("%d*1=%d%n", table, table*1);
+        System.out.printf("%d*2=%d%n", table, table*2);
+        System.out.printf("%d*3=%d%n", table, table*3);
+        System.out.printf("%d*4=%d%n", table, table*4);
+        System.out.printf("%d*5=%d%n", table, table*5);
+        System.out.printf("%d*6=%d%n", table, table*6);
+        System.out.printf("%d*7=%d%n", table, table*7);
+        System.out.printf("%d*8=%d%n", table, table*8);
+        System.out.printf("%d*9=%d%n", table, table*9);
+        System.out.printf("%d*10=%d%n", table, table*10);
+
+//        trial code to understand printf
+        int name = 10;
+        System.out.printf("%d+100=%d%n", name, name+100);
+        String School = "PVP";
+        System.out.printf("My School name is %s", School);
+
+
+
+
+
+
+    }
+}
